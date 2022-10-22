d7cc0bbdba2fbe218b5f180711aa5c8824e32a32
lorenzo de-luca <lorenzodroid@gmail.com>
2022-10-22 18:20:36 +0200

    class refactor

---
+++ it-cost-my-time/Entities/IncomeConfiguration.cs
@@ -0,0 +1,31 @@
+﻿namespace it_cost_my_time.Entities
+{
+    public class IncomeConfiguration
+    {
+        public decimal Income { get; set; } = 30000;
+        public int WorkingDays = 5;
+        public int WorkingHours = 8;
+        public int PayMonths = 13;
+
+        public decimal SavingRate = 50;
+        public decimal Amount = 100;
+        public bool IncomeTypeSelect { get; set; } = true;
+
+        public string IncomeType
+        {
+            get
+            {
+                return (IncomeTypeSelect ?
+                    "Yearly Income (after tax)"
+                :
+                    "Monthly Income (after tax)");
+            }
+
+            set
+            {
+                value = null;
+            }
+        }
+
+    }
+}
---
+++ it-cost-my-time/Services/CalculateService.cs
@@ -0,0 +1,111 @@
+﻿using it_cost_my_time.Entities;
+using Microsoft.Extensions.Configuration;
+using System.Runtime.CompilerServices;
+
+namespace it_cost_my_time.Services
+{
+    public class CalculateService
+    {
+        private readonly IncomeConfiguration IncomeConfiguration;
+
+        public CalculateService(IncomeConfiguration incomeConfiguration)
+        {
+            IncomeConfiguration = incomeConfiguration;
+        }
+
+        public decimal YearlyIncome
+        {
+            get
+            {
+                if (IncomeConfiguration.IncomeTypeSelect)
+                {
+                    return IncomeConfiguration.Income;
+                }
+                else
+                {
+                    return (IncomeConfiguration.Income * IncomeConfiguration.PayMonths);
+                }
+            }
+        }
+
+        public decimal YearlySavings
+        {
+            get
+            {
+                return (decimal)(YearlyIncome / 100) * IncomeConfiguration.SavingRate;
+            }
+        }
+
+        public decimal TotalWorkingDays
+        {
+            get
+            {
+                return (IncomeConfiguration.WorkingDays * 4 * 12);
+            }
+        }
+
+        public decimal TotalWorkingHours
+        {
+            get
+            {
+                return (TotalWorkingDays * IncomeConfiguration.WorkingHours);
+            }
+        }
+
+        public decimal MonthlySavings
+        {
+            get
+            {
+                return (YearlySavings / 12);
+            }
+        }
+
+        public decimal MonthlyIncome
+        {
+            get
+            {
+                return (YearlyIncome / 12);
+            }
+        }
+
+        public decimal DailySaving
+        {
+            get
+            {
+                return YearlySavings / TotalWorkingDays;
+            }
+        }
+
+        public decimal DailyIncome
+        {
+            get
+            {
+                return YearlyIncome / TotalWorkingDays;
+            }
+        }
+
+        public decimal HourlySaving
+        {
+            get
+            {
+                return YearlySavings / TotalWorkingHours;
+            }
+        }
+
+        public decimal HourlyIncome
+        {
+            get
+            {
+                return YearlyIncome / TotalWorkingHours;
+            }
+        }
+
+        public (decimal hours, decimal days)CalculateItemTimeCost(decimal amount) {
+
+            var hours = amount / (YearlySavings / TotalWorkingHours);
+            var days = amount / (YearlySavings / TotalWorkingDays);
+
+            return (hours, days);
+        }
+    }
+}
