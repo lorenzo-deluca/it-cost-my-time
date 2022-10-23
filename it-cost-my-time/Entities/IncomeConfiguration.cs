f6abf5ecaaf7997e0f9c9b76a85af253d100a219
lorenzo de-luca <lorenzodroid@gmail.com>
2022-10-23 11:12:51 +0200

    save configuration in local storage

--- it-cost-my-time/Entities/IncomeConfiguration.cs
+++ it-cost-my-time/Entities/IncomeConfiguration.cs
@@ -1,14 +1,32 @@
-﻿namespace it_cost_my_time.Entities
+﻿using Blazored.LocalStorage;
+
+namespace it_cost_my_time.Entities
 {
     public class IncomeConfiguration
     {
         public decimal Income { get; set; } = 30000;
-        public int WorkingDays = 5;
-        public int WorkingHours = 8;
-        public int PayMonths = 13;
+        public int WorkingDays { get; set; } = 5;
+        public int WorkingHours { get; set; } = 8;
+        public int PayMonths { get; set; } = 13;
+        public decimal SavingRate { get; set; } = 50;
 
-        public decimal SavingRate = 50;
-        public bool IncomeTypeSelect { get; set; } = true;
+        private bool _incomeType;
+
+        public bool IncomeTypeSelect
+        {
+            get
+            {
+                return this._incomeType;
+            }
+
+            set
+            {
+                if (_incomeType != value)
+                    Income = decimal.Round(value ? Income * PayMonths : Income / PayMonths, 2, MidpointRounding.AwayFromZero);
+
+                _incomeType = value;
+            } 
+        }
 
         public string IncomeType
         {
@@ -26,5 +44,14 @@
             }
         }
 
+        public void ReadConfiguration(IncomeConfiguration configuration)
+        {
+            this.Income = configuration.Income;
+            this._incomeType = configuration.IncomeTypeSelect;
+            this.WorkingDays = configuration.WorkingDays;
+            this.WorkingHours = configuration.WorkingHours;
+            this.PayMonths = configuration.PayMonths;
+            this.SavingRate = configuration.SavingRate; 
+        }
     }
 }
--- it-cost-my-time/Program.cs
+++ it-cost-my-time/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
+using Blazored.LocalStorage;
 using it_cost_my_time;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -8,5 +9,8 @@ builder.RootComponents.Add<App>("#app");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
+builder.Services.AddBlazoredLocalStorage(config =>
+       config.JsonSerializerOptions.WriteIndented = true
+   );
 
 await builder.Build().RunAsync();
